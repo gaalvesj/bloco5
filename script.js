ddb47f1bb61e32270e291d29a31931015cd9a720
Gabriel <gabrielalves3654@gmail.com>
2022-05-19 15:58:25 -0300

    Primeiro video do conteudo

---
+++ script.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById('page-title').innerText = 'Gigantes de aço';
+document.getElementById('second-paragraph').innerText = 'Alves é lindo';
+document.getElementById('subtitle').innerText = 'Estudando DOM';
