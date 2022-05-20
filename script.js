95c5c8b83546c115641a2f83868cce78f6d844e8
Gabriel <gabrielalves3654@gmail.com>
2022-05-20 15:17:06 -0300

    segundo exercicio feito

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ console.log(document.getElementById('start').nextElementSibling) // <p>elemento<
 
 // E a prática? Aqui!
 //Crie um irmão para elementoOndeVoceEsta.
-let elemento = document.querySelector('.elementoOndeVoceEsta');
-elemento = document.appendChild('filhoElemento');
+
 
 
 //Crie um filho para elementoOndeVoceEsta.
+let elemento = document.querySelector('.elementoOndeVoceEsta');
+    elemento = document.appendChild('filhoElemento');
+
 //Crie um filho para primeiroFilhoDoFilho.
+let filho1 = document.querySelector(".primeiroFilhoDoFilho");
+    filho1 = document.appendChild("FilhoDoFilho")
+    
 //A partir desse filho criado, acesse terceiroFilho.
