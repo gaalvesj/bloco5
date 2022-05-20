ec3063fce9eb81b16f3ce020d3c4b9b342b06943
Gabriel <gabrielalves3654@gmail.com>
2022-05-20 14:04:51 -0300

    Exercicios feito

---
+++ exercises-javascript-dom/scripts.js
@@ -0,0 +1,18 @@
+let textTitle = document.getElementsByTagName('h1');
+    textTitle[0].innerText = 'Exercício 5.1 - JavaEscript'
+
+let paragrafo = document.getElementsByTagName('p');
+    paragrafo[1].innerText = 'Ser um otimo desenvolvedor back-end'
+
+let quadrado = document.getElementsByClassName('main-content');
+    quadrado[0].style.backgroundColor = 'rgb(76,164,109)'
+
+//let maiusculo = document.getElementsByTagName('p');
+//     for (index = 0; index < quadrado2.length; index += 1)
+//     maiusculo[index].style.fontFamily = 'Arial';
+
+let quadradoMenor = document.querySelector('.center-content');
+    quadradoMenor.style.backgroundColor = 'white'
+
+
+
