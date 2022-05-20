2e9bd685b76996efbf9d736bf457c645120ac7d3
Gabriel <gabrielalves3654@gmail.com>
2022-05-20 15:15:44 -0300

    primeiro exercicio feito

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let paragrafo = document.getElementById('paragraph');
-paragrafo.innerText = 'alves'
+console.log(document.getElementById('start').nextSibling) // nó
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+
+
+
+// E a prática? Aqui!
+//Crie um irmão para elementoOndeVoceEsta.
+let elemento = document.querySelector('.elementoOndeVoceEsta');
+elemento = document.appendChild('filhoElemento');
+
+
+//Crie um filho para elementoOndeVoceEsta.
+//Crie um filho para primeiroFilhoDoFilho.
+//A partir desse filho criado, acesse terceiroFilho.
