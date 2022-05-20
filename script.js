cd3fbf08cc6d3d4433bf96514ff1e7624878862e
Gabriel <gabrielalves3654@gmail.com>
2022-05-20 18:35:12 -0300

    Primeiro item feito

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ console.log(document.getElementById('start').nextElementSibling) // <p>elemento<
 
 // E a prática? Aqui!
 //Crie um irmão para elementoOndeVoceEsta.
-
+  
 
 
 //Crie um filho para elementoOndeVoceEsta.
-let elemento = document.querySelector('.elementoOndeVoceEsta');
-    elemento = document.appendChild('filhoElemento');
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+   let childerelementoOndeVoceEsta =  document.parentElement("section");
+    childerelementoOndeVoceEsta.id = "childerelementoOndeVoceEsta";
+    elementoOndeVoceEsta.appendChild("childerelementoOndeVoceEsta");
+    elementoOndeVoceEsta.innerText = ("Testando!!");
+
+
 
 //Crie um filho para primeiroFilhoDoFilho.
-let filho1 = document.querySelector(".primeiroFilhoDoFilho");
-    filho1 = document.appendChild("FilhoDoFilho")
-    
+//let primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
+  //  filho1 = document.appendChild("FilhoDoFilho")
+
 //A partir desse filho criado, acesse terceiroFilho.
+    //let terceiroFilho = filho1.getElementById('terceiroFilho');
+
