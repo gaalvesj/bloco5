130b016fd7cdcd4561bd23cc1d175dbd59ffcba8
Gabriel <gabrielalves3654@gmail.com>
2022-05-20 19:01:14 -0300

    iniciando os exercicios complementares

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-console.log(document.getElementById('start').nextSibling) // nó
-console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+//console.log(document.getElementById('start').nextSibling) // nó
+//console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
 
 
 
 // E a prática? Aqui!
 //Crie um irmão para elementoOndeVoceEsta.
-  let brotherelementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
-  brotherelementoOndeVoceEsta = document.parentElement("section");
-  brotherelementoOndeVoceEsta.id = 'brotherelementoOndeVoceEsta';
-  elementoOndeVoceEsta.appendChild("brotherelementoOndeVoceEsta");
+//  let brotherelementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+//  brotherelementoOndeVoceEsta = document.parentElement("section");
+//  brotherelementoOndeVoceEsta.id = 'brotherelementoOndeVoceEsta';
+//  elementoOndeVoceEsta.appendChild("brotherelementoOndeVoceEsta");
 
 
 //Crie um filho para elementoOndeVoceEsta.
-let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-   let childerelementoOndeVoceEsta =  document.parentElement("section");
-    childerelementoOndeVoceEsta.id = "childerelementoOndeVoceEsta";
-    elementoOndeVoceEsta.appendChild("childerelementoOndeVoceEsta");
+//let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+//   let childerelementoOndeVoceEsta =  document.parentElement("section");
+//    childerelementoOndeVoceEsta.id = "childerelementoOndeVoceEsta";
+//    elementoOndeVoceEsta.appendChild("childerelementoOndeVoceEsta");
 
 
 
 //Crie um filho para primeiroFilhoDoFilho.
-let primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
-    filho1 = document.parentElement("section");
-    filho1.id = "filho1";
-    primeiroFilhoDoFilho.appendChild("filho1");
+//let primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
+//    filho1 = document.parentElement("section");
+ //   filho1.id = "filho1";
+ //   primeiroFilhoDoFilho.appendChild("filho1");
 
 
 //A partir desse filho criado, acesse terceiroFilho.
-const terceiroFilho = filhoPrimeiroFilhoDoFilho
-       parentElement // primeiroFilhoDoFilho
-       parentElement // elementoOndeVoceEsta
-       nextElementSibling; // terceiroFilho
-    console.log(terceiroFilho);
+//const terceiroFilho = filhoPrimeiroFilhoDoFilho
+//       parentElement // primeiroFilhoDoFilho
+//       parentElement // elementoOndeVoceEsta
+//       nextElementSibling; // terceiroFilho
+//    console.log(terceiroFilho);
+
+
+
+
